319dfc88f6df1bd225a41b05de3bfcdbd356824e
juniorffeestudio <161601367+juniorffeestudio@users.noreply.github.com>
2026-09-20 11:04:19 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from flask import Flask
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GROQ_KEY      = os.getenv("GROQ_KEY")
 GROQ_URL      = "https://api.groq.com/openai/v1/chat/completions"
-GROQ_MODEL    = "llama3-70b-8192"
+GROQ_MODEL    = "llama-3.3-70b-versatile"
 
 CONFIG_FILE    = "config.json"
 
